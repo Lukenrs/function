2ff210b579cc1c5983a6e60a5ef847e1a87f947d
Lukenrs <lukenbr@gmail.com>
2022-01-19 18:38:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let nome
+let number
+let style
+let film
+let music
+let a
+
+function text() {
+    console.log("Alô")
+}
+text()
+
+function name(nome) {
+    console.log(nome)
+}
+name("Luiz")
+
+function more(nome, number, style) {
+    console.log(nome, number, style)
+ 
+}
+more("Luiz", "21", "mpb")
+
+function art(film, music) {
+    console.log(film, music)
+}
+art("Senhor dos Anéis", "Stay With Me")
+
+function triple(a) {
+    a = a*3
+    console.log(a)
+}
+triple(10)
